92179a6e33f71d762ad340df4e25aa1db9a7c0b4
natacon <uikun0113@gmail.com>
2019-03-31 01:57:01 +0900

    fix.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Flask, request
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -39,23 +39,28 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print(str(body))
     app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError as e:
-        print(str(e))
+    except InvalidSignatureError:
+        abort(400)
 
     return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = event.message.text
+    if 'さく' in message:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='なんだ？'))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message))
 
 
 if __name__ == "__main__":
