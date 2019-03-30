4043dcde4d36348ffbbe188cdd178a5d649322a7
natacon <uikun0113@gmail.com>
2019-03-31 00:17:59 +0900

    fix.

--- app.py
+++ app.py
@@ -52,9 +52,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message = event.message.text
+    if 'さく' in message:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='なんだ？'))
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
