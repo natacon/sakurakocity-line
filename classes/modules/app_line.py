ead6049a910963beb4297aaffb33a72558f0cb90
yu.nadawaki <yu-nadawaki@lacras.co.jp>
2019-05-21 13:37:15 +0900

    配列で返却するように変更

--- classes/modules/app_line.py
+++ classes/modules/app_line.py
@@ -30,8 +30,9 @@ handler = WebhookHandler(channel_secret)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = get_message(event.message.text)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+    messages = get_message(event.message.text)
+    for m in messages:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=m))
 
 
 def send_message(reply_token, message):
@@ -40,22 +41,22 @@ def send_message(reply_token, message):
 
 def get_message(text):
     if any(s in text for s in ('さく', 'こちまん', 'らこす', 'ささ', 'さぴ')):
-        return random.choice([
+        return [random.choice([
             'なんだ？',
             'よんだ？',
-        ])
+        ])]
     if any(s in text for s in ('まな', 'まき', 'ちゎ', 'ちわ')):
-        return 'まきちゎ！'
+        return ['まきちゎ！']
     if any(s in text for s in ('ん～', 'ん〜')):
-        return 'ましゃーーー！'
+        return ['ましゃーーー！']
     if any(s in text for s in ('うんこ', 'しっこ', 'しこ')):
-        return random.choice([
+        return [random.choice([
             'うんこゎたしてぃ！',
             'しっこゎたしてぃ！',
             'しこぉ',
-        ])
+        ])]
     if 'お手' in text:
-        return random.choice([
+        return [random.choice([
             'ん？',
             'ぽむむ〜？',
             'なんだ？',
@@ -63,24 +64,24 @@ def get_message(text):
             'おかわりだぞ',
             '立てだぞ',
             'もっと立てだぞ'
-        ])
+        ])]
     if '何の日' in text:
-        return message_util.what_day(text)
+        return [message_util.what_day(text)]
     if '記念日を教えて' in text:
-        return message_util.anniversary(text)
+        return [message_util.anniversary(text)]
     if '付き合って' in text:
-        return message_util.couple_count(text)
+        return [message_util.couple_count(text)]
     if '結婚して' in text:
-        return message_util.marriage_count(text)
+        return [message_util.marriage_count(text)]
     if '何日目カウント' in text:
-        return message_util.day_count(text)
+        return [message_util.day_count(text)]
     if 'ワートリ交換' in text:
         search_results = tweepy_api.search('(交換 OR 缶) (ワートリ OR ワールドトリガー)')
-        s = 'こんなかんじだぞ'
+        sList = ['ついったーしらべたぞ']
         for result in search_results:
-            s += str(result.id)
-            s += result.user.name
-            s += result.text
-            s += str(result.created_at)
-        print("ワートリ交換:", s)
-        return s
+            s = str(result.id)
+            s += '\n' + result.user.name
+            s += '\n' + result.text
+            s += '\n' + str(result.created_at)
+            sList.append(s)
+        return sList
